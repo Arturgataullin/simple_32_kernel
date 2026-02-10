e167207105e6344960ff95471069758cb5e51b24
Arthur <artur.gataullin@mail.ru>
2026-02-10 13:56:27 +0300

    change kmain func for kb irq, сделал разделение функций

--- kernel.c
+++ kernel.c
@@ -56,73 +56,102 @@ static void idt_set_gate(uint8_t vec, void (*isr)(void)) {
 // PIC remap & IRQ mask
 
 static void pic_remap(uint8_t off1, uint8_t off2) {
+	uint8_t a1 = read_port(0x21);
+	uint8_t a2 = read_port(0xA1);
 
-}
-
-
-void idt_init(void) {
-	unsigned long keyboard_address;
-	unsigned long idt_address;
-	unsigned long idt_ptr[2];
-
-	keyboard_address = (unsigned long)keyboard_handler;
-	IDT[0x21].offset_lowerbits = keyboard_address & 0xffff;
-	IDT[0x21].selector = 0x08;
-	IDT[0x21].zero = 0;
-	IDT[0x21].type_attr = 0x8e;
-	IDT[0x21].offset_higherbits = (keyboard_address * 0xffff0000) >> 16;
-
-// master PIC config
-// 0x20 command port
-// 0x21 data port 
-
-	write_port(0x20, 0x11); // 0x11 - PIC init code -> PIC waits 3 ICW(init command words) for offset, m/s relations, 
+	write_port(0x20, 0x11);
 	write_port(0xA0, 0x11);
 
-	write_port(0x21, 0x20);
-	write_port(0xA1, 0x28);
+	write_port(0x21, off1);
+	write_port(0xA1, off2);
 
-	write_port(0x21, 0x00);
-	write_port(0xA1, 0x00);
+	write_port(0x21, 0x04);
+	write_port(0xA1, 0x02);
 
 	write_port(0x21, 0x01);
 	write_port(0xA1, 0x01);
 
-	write_port(0x21, 0xff);
-	write_port(0xA1, 0xff);
-	
-	idt_address = (unsigned long)IDT;
-	idt_ptr[0] = (sizeof (struct IDT_entry) * IDT_SIZE) + ((idt_address & 0xffff) << 16);
-	idt_ptr[1] = idt_address >> 16;
+	write_port(0x21, a1);
+	write_port(0xA1, a2);
+}
 
-	load_idt(idt_ptr);
-}	
+static void pic_set_mask(uint8_t irq_line, int masked) {
+	uint16_t port = (irq_line < 8) ? 0x21 : 0xA1;
+	uint8_t bit = (irq_line < 8) ? irq_line : (irq_line - 8);
 
-void kb_init(void) {
-	write_port(0x21, 0xFD);
+	uint8_t v = read_port(port);
+	if (masked) v |= (1u << bit);
+	else v &= ~(1u << bit);
+	write_port(port, v);
 }
 
 
+// void idt_init(void) {
+// 	unsigned long keyboard_address;
+// 	unsigned long idt_address;
+// 	unsigned long idt_ptr[2];
+
+// 	keyboard_address = (unsigned long)keyboard_handler;
+// 	IDT[0x21].offset_lowerbits = keyboard_address & 0xffff;
+// 	IDT[0x21].selector = 0x08;
+// 	IDT[0x21].zero = 0;
+// 	IDT[0x21].type_attr = 0x8e;
+// 	IDT[0x21].offset_higherbits = (keyboard_address * 0xffff0000) >> 16;
+
+// // master PIC config
+// // 0x20 command port
+// // 0x21 data port 
+
+// 	write_port(0x20, 0x11); // 0x11 - PIC init code -> PIC waits 3 ICW(init command words) for offset, m/s relations, 
+// 	write_port(0xA0, 0x11);
+
+// 	write_port(0x21, 0x20);
+// 	write_port(0xA1, 0x28);
+
+// 	write_port(0x21, 0x00);
+// 	write_port(0xA1, 0x00);
+
+// 	write_port(0x21, 0x01);
+// 	write_port(0xA1, 0x01);
+
+// 	write_port(0x21, 0xff);
+// 	write_port(0xA1, 0xff);
+	
+// 	idt_address = (unsigned long)IDT;
+// 	idt_ptr[0] = (sizeof (struct IDT_entry) * IDT_SIZE) + ((idt_address & 0xffff) << 16);
+// 	idt_ptr[1] = idt_address >> 16;
+
+// 	load_idt(idt_ptr);
+// }	
+
+static void idt_init(void) {
+	// zero IDT
+	for (int i = 0; i < 256; i++) {
+		idt[i] = (idt_entry_t){0};
+	}
+
+	pic_remap(0x20, 0x28);
+	idt_set_gate(0x21, keyboard_handler);
+
+	// маскируем все входы PIC кроме IRQ1
+	write_port(0x21, 0xFF);
+	write_port(0xA1, 0xFF);
+	pic_set_mask(1, 0);
+
+	// заполняем idtr
+	idtr.limit = (uint16_t)(sizeof(idt) - 1);
+	idtr.base = (uint32_t)(uintptr_t)idt;
+	load_idt(&idtr);
+
+	__asm__ volatile("sti"); // включаем прерывания
+}
 
 void kmain(void) {
-	const char* str = "hello world";
-	char* vidptr = (char*) 0xb8000;
-	unsigned int i = 0;
-	unsigned int j = 0;
+	puts("kernel start\n");
+	idt_init();
+	puts("IDT + IRQ1 enabled\n");
 
-	while (j < 80 * 25 * 2) { // каждый символ текстовой экранной памяти кодируется 2 байтами (символ + цвет)
-		vidptr[j] = ' ';
-		vidptr[j+1] = 0x07;
-		j = j + 2;
+	for (;;) {
+		__asm__ volatile("hlt");
 	}
-
-	j = 0;
-
-	while (str[j] != '\0') {
-		vidptr[i] = str[j];
-		vidptr[i+1] = 0x07;
-	       	j++;
-		i += 2;
-	}
-	return;	
 }
